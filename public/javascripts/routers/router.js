00422ee71c56cd43b35286ad9f199aa069d17f0d
infinitec123 <infinitecsolutions@gmail.com>
2013-11-05 14:52:16 +0530

    pagination complete

--- public/javascripts/routers/router.js
+++ public/javascripts/routers/router.js
@@ -16,11 +16,13 @@ var app = app || {};
             "teasers/new": "addTeaser",
             "teasers/edit/:id":"editTeaserDetails",
 			"teasers/:id":"teaserDetails",
-            "filter/:id": "manageFilter",
+            //"filter/:id": "manageFilter",
+            "teasers/page/:page": "initialLoad",
             "": "initialLoad"
 	},
 
-    manageFilter: function(_filter){
+
+    manageFilter: function(_filter){ //
         console.log("Received request to filter" + _filter);
         
         //console.log(result); 
@@ -73,59 +75,61 @@ var app = app || {};
              this.currentView = view;
     },
 
-    initialLoad: function(){
-        console.log('Inside router for intial load');  
-        app.masterList = new app.Teasers();    
-        //app.teasersList = new app.Teasers();
-        var self = this;   
-        app.masterList.fetch({
-        success:function () {
-                app.teasersList = app.masterList;
-                self.mainLoad(app.teasersList);    
+    initialLoad: function(page){
+        this.p = page ? parseInt(page, 10) : 1;
+        console.log('Inside router for intial load for page: ' + this.p + ' With filter:: ' + app.chosenFilter);  
+
+        if(!app.masterList){
+            app.masterList = new app.Teasers();    
+            //app.teasersList = new app.Teasers();
+            var self = this;   
+            app.masterList.fetch({
+                success:function () {
+                console.log("Fetched data from server!"); 
+                if(app.chosenFilter =="All") {
+                    app.teasersList = app.masterList;
+                } else {
+                    var result = app.masterList.where({category: _filter});
+                    app.teasersList = new app.Teasers(result); 
+                }
+                self.mainLoad(app.teasersList);             
             }
-        });
+            });
+        } else {
+
+                if(app.chosenFilter =="All") {
+                    app.teasersList = app.masterList;
+                } else {
+                    var result = app.masterList.where({category: app.chosenFilter});
+                    app.teasersList = new app.Teasers(result); 
+                }
+                this.mainLoad(app.teasersList);   
+        }
     }, 
 
     mainLoad: function(_collection){
-        console.log('Inside main load'); 
-                this.teasersListView = new app.TeasersListView({model:_collection}); 
-                $('#sidebar').html(this.teasersListView.render().el);
-                $("#filter").val(app.chosenFilter);
+        //console.log('Inside main load'); 
 
-                if(!app.addTeaserRequest) { //main load first time with or without id.
-                    //console.log("Should not have come here!" + !app.addTeaserRequest );
-                    if(this.requestedId) {
+        this.teasersListView = new app.TeasersListView({page:this.p, model:_collection}); 
+        $('#sidebar').html(this.teasersListView.render().el);
+        $("#filter").val(app.chosenFilter);
+
+            if(!app.addTeaserRequest) { //main load first time with or without id.
+                //console.log("This is not an add request");
+                if(this.requestedId) {
                         this.teaserDetails(this.requestedId);
-                    } else {
-                        if(app.teasersList.length > 0)  this.teaserDetails(app.teasersList.at(0).get('_id'));     
+                } else {
+                        if(app.teasersList.length > 0) {
+                            //var Pos = Math.min(startPos + 4, len);
+                            this.teaserDetails(app.teasersList.at((this.p-1)*4).get('_id')); 
+                        }     
                     }  
-                } else { //Its main load for add
-                        this.teaserNewView = new app.TeasersNewView({model: new app.Teaser()});
-                        $('#mainbar').html(this.teaserNewView.render().el);
-                        app.addTeaserRequest = false;
-                }
+            } else { //Its main load for add
+                    this.teaserNewView = new app.TeasersNewView({model: new app.Teaser()});
+                    $('#mainbar').html(this.teaserNewView.render().el);
+                    app.addTeaserRequest = false;
+            }
         }, 
-
-/*    teaserDetails: function(id){
-        if(app.reload){
-            //console.log("Reload request;");
-            app.reload = false;
-            this.requestedId = id;
-            this.initialLoad();
-            utils.showAlert('Success!', 'Teaser saved successfully', 'alert-success');
-
-        } else {
-            if(app.teasersList){
-                this.teaser = app.teasersList.get(id);
-                if(this.teasersView)  this.teasersView.remove();                
-                this.teasersView = new app.TeasersView({model: this.teaser});
-                $('#mainbar').html(this.teasersView.render().el);
-        } else {
-                this.requestedId = id;
-                this.initialLoad();
-                }
-        }
-    }, */
 
     teaserDetails: function(id){
 
@@ -135,7 +139,7 @@ var app = app || {};
 
          if(app.teasersList){
                 this.teaser = app.teasersList.get(id);
-                if(this.teasersView)  this.teasersView.remove();                
+                if(this.teasersView)  this.teasersView.close();                
                 this.teasersView = new app.TeasersView({model: this.teaser});
                 $('#mainbar').html(this.teasersView.render().el);
         } else {
--- public/javascripts/views/teasersListView.js
+++ public/javascripts/views/teasersListView.js
@@ -8,18 +8,14 @@ app.TeasersListView = Backbone.View.extend({
     //tagName: 'ul',
     template:_.template($('#sidebar-template').html()),
 
-    initialize: function() {
+    initialize: function(attrs) {
 
         var self = this;
         this.listenTo( app.teasersList, 'add', this.renderTeaser );
         this.listenTo( app.teasersList, 'reset', this.render);
-        this.listenTo( app.teasersList, 'filtered', this.render2);
-        //this.listenTo( app.teasersList, 'sync', this.renderOnSync);
-    /*    this.model.bind("add", function(item){
-                    console.log(item.toJSON());
-                    console.log(item.get('_id'));
-        }); */
-        },
+        this.options = attrs;
+
+    },
 
     events:{
         'change select#filter': 'triggerFilter'
@@ -27,47 +23,41 @@ app.TeasersListView = Backbone.View.extend({
 
     triggerFilter: function(e){
         app.chosenFilter = $(e.currentTarget).val();
-        app.TeaserRouter.navigate('filter/' + app.chosenFilter, {trigger: true});
-    },
-
-    render2: function(){
-        console.log("filtered received");
-        console.log(this.teasersList);
+        console.log("ListView: Filtering request for " + app.chosenFilter );
+        console.log(Backbone.history.fragment);
+        app.TeaserRouter.navigate('teasers/page/1', {trigger: true});
     },
 
     render:function (eventName) {
          
+        //var teasers = this.model.models;
+        var len = this.model.models.length;
+        //console.log(this.page);
+        var startPos = (parseInt(this.options.page) - 1) * 4;
+        var endPos = Math.min(startPos + 4, len);
+        //console.log("startPos" + startPos + "length" + len + "endPos" + endPos);
+
          $(this.el).html(this.template);
          $(this.el).append("<ul>");
 
-        _.each(this.model.models, function (teaser) {
-            $(this.el).append(new app.TeaserListItemView({model:teaser}).render().el);
-        }, this);
+         for (var i = startPos; i < endPos; i++) {
+            $(this.el).append(new app.TeaserListItemView({model:this.model.models[i]}).render().el);
+         } 
 
         $(this.el).append("</ul>");
+        $(this.el).append(new app.Paginator({model: this.model, page: this.page}).render().el);
         return this;
-        //$('#sidebar').html($(this.el));
-    },
 
-    renderOnSync:function (_collection, _resp, _options) {
-        console.log("Sync detected");
-        _.each(_collection, function (teaser) {
-            $(this.el).append(new app.TeaserListItemView({model:teaser}).render().el);
-        }, this);
-        return this;
+       // _.each(this.model.models, function (teaser) {
+            //$(this.el).append(new app.TeaserListItemView({model:teaser}).render().el);
+       // }, this);
+
+
+
     },
 
     renderTeaser: function(item){
-        //console.log("Will add new teaser to the list");
-        //console.log(item.toJSON());
-        //console.log(item.get('_id'));
-        //console.log(item.get('id'));
         $(this.el).append(new app.TeaserListItemView({model:item}).render().el);
-        //this.$el.append( new app.TeaserListItemView({model:item}).render().el );
-        //$(this.el).append(new app.TeaserListItemView({model:item}).render().el);
-        //$('#sidebar').html($(this.el));
-        //console.log($(this.el));
-        //return this;
     },
 
     close: function(){
